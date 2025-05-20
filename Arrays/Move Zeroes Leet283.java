56ab9454b7fdb4a22ed632250d7450c246db1e86
Firoz-S <firozsindgi5@gmail.com>
2025-05-20 22:45:08 +0530

    Update Move Zeroes Leet283.java
    used bubble sort to move zeroes to end .

--- Arrays/Move Zeroes Leet283.java
+++ Arrays/Move Zeroes Leet283.java
@@ -1,18 +1,33 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j;
-        for(int i=0;i<nums.length;i++){
-            j=i;
-            while(j<nums.length){
-              if(nums[j]==0){
-                 j++;
-              }
-              else{
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                break;
-              }
+        // int j;
+        // for(int i=0;i<nums.length;i++){
+        //     j=i;
+        //     while(j<nums.length){
+        //       if(nums[j]==0){
+        //          j++;
+        //       }
+        //       else{
+        //         int temp=nums[i];
+        //         nums[i]=nums[j];
+        //         nums[j]=temp;
+        //         break;
+        //       }
+        //     }
+        // }
+        int nooz=0;
+        for(int ele:nums){
+            if(ele==0){
+                nooz++;
+            }
+        }
+        for(int i=0;i<nooz;i++){
+            for(int j=0;j<nums.length-1-i;j++){
+                if(nums[j]==0){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
             }
         }
     }
